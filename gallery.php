8c50227daebf5485d16c714d31ddd724b6796983
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2021-12-25 00:05:18 +0100

    Add gallery pages

--- gallery.php
+++ gallery.php
@@ -2,8 +2,18 @@
 
 require_once 'functions.php';
 
+$IMAGES_PER_PAGE = 2;
+$startImage = 0;
+if (!empty($_GET['page'])) {
+    $startImage = ((int)$_GET['page']-1) * $IMAGES_PER_PAGE;
+}
+
 $db = get_db();
 $products = $db->products->find();
+$products_array = iterator_to_array($products);
+$images_amount = count($products_array);
+$last_index = ($images_amount < $startImage+$IMAGES_PER_PAGE) ? $images_amount : $startImage+$IMAGES_PER_PAGE;
+$pages = ($images_amount+1)/$IMAGES_PER_PAGE;
 
 ?>
 <!DOCTYPE html>
@@ -15,19 +25,26 @@ $products = $db->products->find();
 <body>
 
 <?php if ($db->products->count()): ?>
-    <?php foreach ($products as $index=>$product): ?>
+    <?php for ($i=$startImage; $i<$last_index; $i++): ?>
     <div class="gallery_image">
-        <h1><?= $index+1 . ". " . $product['name'] ?></h1>
-        <h3><?= $product['author'] ?></h3>
-        <a href="view.php?id=<?= $product['_id'] ?>">
-            <img src="<?= "/images/miniature_" . $product['filename'] ?>"</img> </br>
+        <h1><?= $i+1 . ". " . $products_array[$i]['name'] ?></h1>
+        <h3><?= $products_array[$i]['author'] ?></h3>
+        <a href="view.php?id=<?= $products_array[$i]['_id'] ?>">
+            <img src="<?= "/images/miniature_" . $products_array[$i]['filename'] ?>"</img> </br>
         </a>
     </div>
-    <?php endforeach ?>
+    <?php endfor ?>
 <?php else: ?>
     Brak produktów
 <?php endif ?>
 
+<?php 
+for ($i=1; $i<=$pages; $i++) {
+    echo "<span><a href='?page=$i'>$i</a> </span>";
+}
+?>
+</br>   
+</br>   
 <a href="index.php">&laquo; Wróæ</a>
 
 </body>
