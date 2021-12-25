bf8cf901b6decb914e1e46cc5f256546bcbdafec
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2021-12-25 12:02:06 +0100

    Fix cookie indexing

--- removecookie.php
+++ removecookie.php
@@ -6,7 +6,18 @@ if (isset($_POST['name'])) {
 	if (is_array($cookie)) {
 		$index = array_search($cookieValue, $cookie);
 		unset($_COOKIE[$cookieName][$index]); 
-		setcookie($cookieName . "[" . $index . "]", null, -1, '/'); 
+		setcookie($cookieName . "[" . $index . "]", null, -1, '/');
+
+		// reset cookies indexing
+		$newCookies = array_map('array_values', $_COOKIE);
+		foreach ($_COOKIE[$cookieName] as $key => $value) {
+			setcookie( $cookieName . "[" . $key . "]", FALSE, -1, '/' );
+			unset($_COOKIE[$cookieName][$key]);
+		}
+		
+		foreach ($newCookies[$cookieName] as $key=>$value) {
+			setcookie($cookieName . "[" . $key . "]", $value, -1, '/');
+		}
 	}
 	else {
 		unset($_COOKIE[$cookieName]); 
